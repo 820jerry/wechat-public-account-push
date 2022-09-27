69af75f61f6995c779c7d26bff9e196fe2e78ce6
820jerry <114337517+820jerry@users.noreply.github.com>
2022-09-27 08:11:44 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -65,9 +65,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '老婆0',
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ol36d6ZzOUqSHbNtwzF9My-W4iig',
+      id: 'ol36d6bDswHI_NGuTu0Rb9MlMfWQ','ol36d6ZzOUqSHbNtwzF9My-W4iig',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: '0UFAm5m7ffU0Fxpi0mmveGpiuatkamNHV_n4uPO1oLg',
+      useTemplateId: '_rpz22G2CLnJRk3qdFRAbBxAbW7h-L4H38qZd4RHEb4',
       // 所在省份
       province: '广西',
       // 所在城市
