22c25dfada5e357bc47e27cb0d5c0c8463872617
820jerry <114337517+820jerry@users.noreply.github.com>
2022-12-11 15:50:01 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -84,6 +84,7 @@ const USER_CONFIG = {
         {
           type: '*生日', name: '乖乖', year: '2003', date: '09-09',
         },
+        ],
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
